9801f4610b1fe5afac728bf2d0fa4d424943d1b0
tbkong2 <tony2002kong@gmail.com>
2026-02-03 16:22:19 -0700

    works with memtest not quite with thread test

--- main.c
+++ main.c
@@ -11,26 +11,37 @@
 
 #include "common.h"
 #include <unistd.h>
+#include <sys/time.h>
 
 
 //Global Vars
 char** stringArray;
 pthread_rwlock_t** rwlockArray;
 int numOfStringsGlobal;
+double* times;
+struct timeval* startTimes = NULL;
+struct timeval* endTime = NULL;
 
+typedef struct {
+    int clientFileDescriptor;
+    int requestID;
+} Threadargs;
 
-
-void *serverTask(void *args){
-    int clientFileDescriptor = (int)(long)args;
+void *serverTask(void *arg){
+    Threadargs *args = (Threadargs *)arg;
+    int clientFd = args->clientFileDescriptor;
+    int requestID = args->requestID;
+    free(arg);
     char msg[COM_BUFF_SIZE];
+
     
     ClientRequest request;
     //add exception handling
     int total = 0;
     while (total < COM_BUFF_SIZE) {
-        int r = read(clientFileDescriptor, msg + total, COM_BUFF_SIZE - total);
+        int r = read(clientFd, msg + total, COM_BUFF_SIZE - total);
         if (r <= 0) {
-            close(clientFileDescriptor);
+            close(clientFd);
             return NULL;
         }
         total += r;
@@ -40,7 +51,7 @@ void *serverTask(void *args){
     msg[COM_BUFF_SIZE-1] = '\0';
     ParseMsg(msg,&request);
     if (request.pos < 0 || request.pos >= numOfStringsGlobal) {
-        close(clientFileDescriptor);
+        close(clientFd);
         return NULL;
     }
 
@@ -49,19 +60,21 @@ void *serverTask(void *args){
         pthread_rwlock_rdlock(rwlockArray[request.pos]);
         getContent(msg, request.pos, stringArray);
         pthread_rwlock_unlock(rwlockArray[request.pos]);
-        write(clientFileDescriptor,msg,COM_BUFF_SIZE);
+        write(clientFd,msg,COM_BUFF_SIZE);
     } else {
 
         pthread_rwlock_wrlock(rwlockArray[request.pos]);
         setContent(request.msg, request.pos, stringArray);
         getContent(msg, request.pos, stringArray);
         pthread_rwlock_unlock(rwlockArray[request.pos]);
-        write(clientFileDescriptor,msg,COM_BUFF_SIZE);
+        write(clientFd,msg,COM_BUFF_SIZE);
     }
-
-
-
-    close(clientFileDescriptor);
+    gettimeofday(&endTime[requestID], NULL);
+    times[requestID] =
+    (endTime[requestID].tv_sec - startTimes[requestID].tv_sec) * 1e6 +
+    (endTime[requestID].tv_usec - startTimes[requestID].tv_usec);
+    printf("Request %d completed in %lf microseconds\n", requestID, times[requestID]);
+    close(clientFd);
 
     return NULL;
     
@@ -72,10 +85,8 @@ int main(int argc, char* argv[]){
     int numOfStrings = strtol(argv[1], NULL, 10);
     numOfStringsGlobal = numOfStrings;
     rwlockArray = (pthread_rwlock_t**) malloc(numOfStrings * sizeof(pthread_rwlock_t*));
-    for (int i = 0; i < numOfStrings; i++){
-        rwlockArray[i] = (pthread_rwlock_t*) malloc(sizeof(pthread_rwlock_t));
-        pthread_rwlock_init(rwlockArray[i], NULL);
-    }
+
+    times = (double*) malloc(COM_NUM_REQUEST * sizeof(double));
     char *serverIP = argv[2];
     int serverPort = strtol(argv[3], NULL, 10);
 
@@ -88,11 +99,15 @@ int main(int argc, char* argv[]){
     sockAddr.sin_family=AF_INET;
 
     pthread_t* threadHandles = malloc(COM_NUM_REQUEST*sizeof(pthread_t));
+    startTimes = malloc(COM_NUM_REQUEST * sizeof(struct timeval));
+    endTime = malloc(COM_NUM_REQUEST * sizeof(struct timeval)); 
 
     stringArray = (char**) malloc(numOfStrings * sizeof(char*));
     for (int i = 0; i < numOfStrings; i ++){
         stringArray[i] = (char*) malloc(COM_BUFF_SIZE * sizeof(char));
         sprintf(stringArray[i], "String %d: the initial value", i);
+        rwlockArray[i] = (pthread_rwlock_t*) malloc(sizeof(pthread_rwlock_t));
+        pthread_rwlock_init(rwlockArray[i], NULL);
     }
 
 
@@ -102,10 +117,14 @@ int main(int argc, char* argv[]){
         listen(serverFileDescriptor, COM_NUM_REQUEST*2); //I don't know what to do with this magic number
         
         for(int i = 0; i < COM_NUM_REQUEST; i++){
-        
-            clientFileDescriptor = accept(serverFileDescriptor, NULL, NULL);
+                    
+            clientFileDescriptor = accept(serverFileDescriptor, NULL, NULL);            
             printf("Connected to client %d\n",clientFileDescriptor);
-            pthread_create(&threadHandles[i], NULL, serverTask, (void *)(long)clientFileDescriptor);
+            Threadargs* args = malloc(sizeof(Threadargs)); 
+            args->clientFileDescriptor = clientFileDescriptor;
+            args->requestID = i;
+            gettimeofday(&startTimes[i], NULL);            
+            pthread_create(&threadHandles[i], NULL, serverTask, args);
         }
 
         for(int i = 0; i < COM_NUM_REQUEST; i++)
@@ -116,17 +135,20 @@ int main(int argc, char* argv[]){
     else{
         printf("socket creation failed\n");
     }
+    saveTimes(times, COM_NUM_REQUEST);
 
     //Cleanup
-    free(threadHandles);
-    for (int i = 0; i < numOfStrings; i ++)
-        free(stringArray[i]);
-    free(stringArray);
     for (int i = 0; i < numOfStrings; i++) {
         pthread_rwlock_destroy(rwlockArray[i]);
         free(rwlockArray[i]);
+        free(stringArray[i]);
     }
+    free(startTimes);
+    free(endTime);
+    free(times);
     free(rwlockArray);
+    free(threadHandles);
+    free(stringArray);
 
 
     return 0;
