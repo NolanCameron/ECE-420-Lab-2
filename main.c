edd21b5c4a9b9691ff8b74fc5c12e0308a513dd1
tbkong2 <tony2002kong@gmail.com>
2026-02-03 01:04:07 -0700

    Made a version I think works with client.c

--- main.c
+++ main.c
@@ -8,10 +8,17 @@
 #include <errno.h>
 #include <stdlib.h>
 
+
 #include "common.h"
+#include <unistd.h>
+
 
 //Global Vars
 char** stringArray;
+pthread_rwlock_t** rwlockArray;
+int numOfStringsGlobal;
+
+
 
 void *serverTask(void *args){
     int clientFileDescriptor = (int)(long)args;
@@ -19,10 +26,40 @@ void *serverTask(void *args){
     
     ClientRequest request;
     //add exception handling
-    read(clientFileDescriptor,msg,12);
+    int total = 0;
+    while (total < COM_BUFF_SIZE) {
+        int r = read(clientFileDescriptor, msg + total, COM_BUFF_SIZE - total);
+        if (r <= 0) {
+            close(clientFileDescriptor);
+            return NULL;
+        }
+        total += r;
+    }
+
     printf("Reading from client...\n");
+    msg[COM_BUFF_SIZE-1] = '\0';
     ParseMsg(msg,&request);
+    if (request.pos < 0 || request.pos >= numOfStringsGlobal) {
+        close(clientFileDescriptor);
+        return NULL;
+    }
+
     printf("Recieved: %d %d %s\n",request.pos,request.is_read,request.msg);
+    if (request.is_read){
+        pthread_rwlock_rdlock(rwlockArray[request.pos]);
+        getContent(msg, request.pos, stringArray);
+        pthread_rwlock_unlock(rwlockArray[request.pos]);
+        write(clientFileDescriptor,msg,COM_BUFF_SIZE);
+    } else {
+
+        pthread_rwlock_wrlock(rwlockArray[request.pos]);
+        setContent(request.msg, request.pos, stringArray);
+        getContent(msg, request.pos, stringArray);
+        pthread_rwlock_unlock(rwlockArray[request.pos]);
+        write(clientFileDescriptor,msg,COM_BUFF_SIZE);
+    }
+
+
 
     close(clientFileDescriptor);
 
@@ -31,8 +68,14 @@ void *serverTask(void *args){
 }
 
 int main(int argc, char* argv[]){
-
+       
     int numOfStrings = strtol(argv[1], NULL, 10);
+    numOfStringsGlobal = numOfStrings;
+    rwlockArray = (pthread_rwlock_t**) malloc(numOfStrings * sizeof(pthread_rwlock_t*));
+    for (int i = 0; i < numOfStrings; i++){
+        rwlockArray[i] = (pthread_rwlock_t*) malloc(sizeof(pthread_rwlock_t));
+        pthread_rwlock_init(rwlockArray[i], NULL);
+    }
     char *serverIP = argv[2];
     int serverPort = strtol(argv[3], NULL, 10);
 
@@ -79,6 +122,12 @@ int main(int argc, char* argv[]){
     for (int i = 0; i < numOfStrings; i ++)
         free(stringArray[i]);
     free(stringArray);
+    for (int i = 0; i < numOfStrings; i++) {
+        pthread_rwlock_destroy(rwlockArray[i]);
+        free(rwlockArray[i]);
+    }
+    free(rwlockArray);
+
 
     return 0;
 
